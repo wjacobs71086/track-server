c594b48676ffc3feace485d995221ce4223d36d3
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-23 13:23:49 -0700

    server auth is now 100% working

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 /* This is the file that creates the Schema on the MongoDB database. You do NOT have to setup on MongoDB directly as this Schema will tell it the requirements it needs.
 */
 const userSchema = new mongoose.Schema({
@@ -13,6 +13,32 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
+// using a keyword "function" instead of arrow function because the .pre uses keyword "this" to reference the user and not the global component.
+userSchema.pre('save', function(next) {
+  const user = this;
+  if(!user.isModified('password')) return next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) return next(err);
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    })
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if(err) return reject(err);
+      if (!isMatch) return reject(false);
+      // if there is not an error, and the password matches then we sent a resolve from our promise.
+      resolve(true);
+    });
+  })
+};
 
 
 mongoose.model('User', userSchema);
--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -4,10 +4,9 @@ const router = express.Router();
 const User = mongoose.model("User");
 const jwt = require('jsonwebtoken');
 
-router.post("/signup", async (req, res) => {
+router.post("/signup", async (req, res, next) => {
   const { email, password } = req.body;
   const user = new User({ email, password });
-  
   try {
     // This is an async operation with MongoDB
     await user.save();
@@ -16,8 +15,26 @@ router.post("/signup", async (req, res) => {
   } catch (err) {
     return res.status(422).send(err.message);
   }
-
-  res.send("Created");
 });
+
+router.post('/signin', async (req,res) => {
+ const { email, password } = req.body;
+  if( !email || !password ) return res.status(422).send({error: 'Missing credentials'});
+
+  const user = await User.findOne({email});
+  if(!user) return res.status(404).send({error: 'Invalid Credentials'});
+  
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({userId: user._id}, 'VoodooRanger')
+    res.send({token});
+  } catch (err) {
+    return res.status(422).send({error: 'Invalid Credentials'})
+  }
+  
+
+
+});
+
 
 module.exports = router;
