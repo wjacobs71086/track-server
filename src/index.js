5397e2a22a29f4f1326c320fb63b5faf05cd4f2a
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-23 09:14:21 -0700

    added MongoDB connection and nodemon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 const app = express();
 
+// String that allows the connection to the MongoDB. Created by generating a new cluster.
+const mongoUri = 'mongodb+srv://admin:Password123!@cluster0-dm14h.mongodb.net/test?retryWrites=true&w=majority';
+
+// These avoid common error messages. unsure what they do.
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true 
+});
+
+// Creates a listener that when the DB is connected, executes the callback.
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB instance.')
+});
+
+// This will show an error when one appears
+mongoose.connection.on('error', (e) => {
+  console.log({ErrorMessage: e});
+});
 
 app.get('/', (req, res) => {
   res.send('hello, world');
